407d67c3fc58c9522d836762251d545ed08d53e5
TharushaK01 <tharushakavinda01@gmail.com>
2026-06-01 10:29:29 +0530

    dashboard backgorund updated

--- components/home/DashboardPreview.tsx
+++ components/home/DashboardPreview.tsx
@@ -55,7 +55,7 @@ export default function YourSectionComponent() {
           alt=""
           fill
           priority
-          className="object-cover opacity-80"
+          className="object-cover opacity-100%"
         />
       </div>
 
