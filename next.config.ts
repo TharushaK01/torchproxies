0cc6bdf060f086749d57e4f297fbf32d8d1eeb48
TharushaK01 <tharushakavinda01@gmail.com>
2026-09-07 14:58:14 +0530

    Fix the CSP String Format

--- next.config.ts
+++ next.config.ts
@@ -24,20 +24,20 @@
 
 // img-src 'self' data: blob: https://cms.torchproxies.com;
 // The Content Security Policy directive
-const ContentSecurityPolicy: string = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://*.doubleclick.net https://www.googleadservices.com https://*.google.com https://*.google.lk;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://torchproxies.com;
-  img-src 'self' data: blob: https: https://*.doubleclick.net https://*.google.com https://*.google.lk;
-  font-src 'self' https://fonts.gstatic.com;
-  connect-src 'self' https://*.google.com https://*.google.lk https://www.google-analytics.com https://*.google-analytics.com https://analytics.google.com https://www.googleadservices.com https://*.doubleclick.net https://cms.torchproxies.com;
-  frame-src 'self' https://*.doubleclick.net https://*.google.com;
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  upgrade-insecure-requests;
-`;
+const ContentSecurityPolicy: string = [
+  "default-src 'self';",
+  "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://*.doubleclick.net https://www.googleadservices.com https://*.google.com https://*.google.lk;",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://torchproxies.com;",
+  "img-src 'self' data: blob: https: https://*.doubleclick.net https://*.google.com https://*.google.lk;",
+  "font-src 'self' https://fonts.gstatic.com;",
+  "connect-src 'self' https://*.google.com https://*.google.lk https://www.google-analytics.com https://*.google-analytics.com https://analytics.google.com https://www.googleadservices.com https://*.doubleclick.net https://cms.torchproxies.com;",
+  "frame-src 'self' https://*.doubleclick.net https://*.google.com;",
+  "object-src 'none';",
+  "base-uri 'self';",
+  "form-action 'self';",
+  "frame-ancestors 'none';",
+  "upgrade-insecure-requests;",
+].join(" ");
 // .replace(/\s{2,}/g, " ")
 // .trim();
 
